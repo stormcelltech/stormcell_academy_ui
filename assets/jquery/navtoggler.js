5ac2cc33256a7b584ecede0d4be90ab244e23ce4
cyber4kinqslee <amakirisoibibo@gmail.comgit config --global user.name cyber4kinqslee>
2022-05-22 23:44:00 +0100

    first commit

---
+++ assets/jquery/navtoggler.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    // $('.first-button').on('click', function () {
+          
+    //   $('.animated-icon1').toggleClass('open');
+    // });
+    // $('.second-button').on('click', function () {
+    
+    //   $('.animated-icon2').toggleClass('open');
+    // });
+    $('.third-button').on('click', function () {
+    
+      $('.menu-icon3').toggleClass('open');
+    });
+    });
